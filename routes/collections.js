13c9b44bde0e8b3799f1d3ca060e3cd5ecd5e621
Machaelmus <michaeljtufo@gmail.com>
2021-06-02 18:59:57 -0400

    displaying correct info

--- routes/collections.js
+++ routes/collections.js
@@ -1,21 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
+const {restoreUser} = require('../auth')
 const {User, Collection, Trail} = require('../db/models')
 const {asyncHandler} = require('../utils');
 
 /* GET THE COLLECTION OF TRAILS */
-router.get('/all', asyncHandler(async (req, res) => {
-    // const user = res.locals.user.id;
-    // const { user_id, trail_id, visited, want_to_visit } = req.body;
-
+router.get('/all', restoreUser, asyncHandler(async (req, res) => {
     const collectionOfTrails = await Collection.findAll({
         include: Trail
     });
-    console.log(collectionOfTrails)
-    // req.session.save(() => {
         res.render('collections', {collectionOfTrails})
-    // });
 }));
 
 // router.post('/all/:id', asyncHandler(async (req, res) => {
