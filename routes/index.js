8a1cc48c5d9407b188aaa257b755e57cefe5864b
Geoffrey <geoffreyyang@gmail.com>
2021-06-02 18:27:14 -0400

    working on states/state_code

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,25 @@
+//external Packages
 var express = require('express');
 var router = express.Router();
 //internal packages
-const { State } = require('../db/models')
+const { State , Trail} = require('../db/models')
 const { asyncHandler } = require('../utils')
 
 /* GET home page not logged in. */
 router.get('/', asyncHandler(async (req, res, next) => {
   const states = await State.findAll()
-  console.log(states[0]);
   res.render('landing', {
     title: 'Welcome to Good Trails',
     states
   });
 }));
+
+//********************************
+// Misc API routes
+
+// GET trails in a given state_code
+router.get('/states/:state_code', asyncHandler(async (req, res) => {
+
+}))
 
 module.exports = router;
