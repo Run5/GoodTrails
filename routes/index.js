4aa11a06761e93c911f2c05904ca6cc5d767881b
Geoffrey <geoffreyyang@gmail.com>
2021-06-02 13:25:17 -0400

    using array of states

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const {stateCodes} = require('../utils')
+const {stateCodes} = require('../utils') 
 
 /* GET home page not logged in. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('landing', {
     title: 'Welcome to Good Trails',
     states: stateCodes
