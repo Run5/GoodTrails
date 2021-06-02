a212aed1f77eaf961a141b0cf28610efaf2eff37
Geoffrey <geoffreyyang@gmail.com>
2021-06-02 19:40:00 -0400

    ready to merge

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 //internal packages
-const { State , Trail} = require('../db/models')
+const { State, Trail } = require('../db/models')
 const { asyncHandler } = require('../utils')
 
 /* GET home page not logged in. */
@@ -16,9 +16,28 @@ router.get('/', asyncHandler(async (req, res, next) => {
 
 //********************************
 // Misc API routes
+//********************************
+
 
 // GET trails in a given state_code
+// do we need csrfProtection on this route?
+// gy: this doesn't need to be an api route.
 router.get('/states/:state_code', asyncHandler(async (req, res) => {
+
+  const states = await State.findAll({
+    include: Trail,
+    where: { state_code: req.params.state_code }
+  })
+  console.log("Arizona!!!!", trails);
+  if (!states) {
+    let error = {
+      status: 404,
+      typeOf: "state",
+      message: "no trails in this state"
+    };
+    next(error)
+  }
+  res.render('states', { state })
 
 }))
 
