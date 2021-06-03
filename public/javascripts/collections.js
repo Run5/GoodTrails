73520a334d6a3db5375c44c6c96c0c50cb2b0660
Machaelmus <michaeljtufo@gmail.com>
2021-06-03 14:07:37 -0400

    added error handling to both visited and all

--- public/javascripts/collections.js
+++ public/javascripts/collections.js
@@ -3,41 +3,58 @@ document.addEventListener("DOMContentLoaded", async () => {
                           //ALL TRAILS
   const allButton = document.querySelector('.allButton');
   const allContainer = document.querySelector('.allContainer');
+  const actualError = document.querySelector('.actualError');
+  const visitedButton = document.querySelector('.visitedButton');
 
   allButton.addEventListener('click', async (e) => {
-    const res = await fetch('/my-trails/all')
 
-    if(allButton.classList.contains('on')) {
-      allButton.classList.remove('on')
-      allButton.classList.add('off')
-    } else {
-      allButton.classList.remove('off')
-      allButton.classList.add('on')
+    try {
+
+      const res = await fetch('/my-trails/all');
+
+      if(allButton.classList.contains('on')) {
+        allButton.classList.remove('on');
+        allButton.classList.add('off');
+      } else {
+        allButton.classList.remove('off');
+        allButton.classList.add('on');
+      }
+
+      const allTrails = await res.json()
+      allContainer.innerHTML = [allTrails[0].Trail.name];
+      console.log(allTrails);
+
+    } catch (err) {
+
+      console.log('Sorry, we couldn\'t find any trails!', err);
+      actualError.innerHTML = 'Sorry, we couldn\'t find any trails that you\'ve visited';
     }
-
-    const allTrails = await res.json()
-    allContainer.innerHTML = [allTrails[0].Trail.name];
-    console.log(allTrails)
   })
 
 //_____________________________________________________________________________
                         //VISITED TRAILS
-  const visitedButton = document.querySelector('.visitedButton');
-
   visitedButton.addEventListener('click', async (e) => {
-    const res = await fetch('/my-trails/visited');
 
-    if(visitedButton.classList.contains('on')) {
-      visitedButton.classList.remove('on')
-      visitedButton.classList.add('off')
-    } else {
-      visitedButton.classList.remove('off')
-      visitedButton.classList.add('on')
+    try {
+
+      const res = await fetch('/my-trails/visited');
+
+      if(visitedButton.classList.contains('on')) {
+        visitedButton.classList.remove('on');
+        visitedButton.classList.add('off');
+      } else {
+        visitedButton.classList.remove('off');
+        visitedButton.classList.add('on');
+      }
+
+      const visitedTrails = await res.json();
+      allContainer.innerHTML = [visitedTrails[0].Trail.name];
+      console.log(visitedTrails);
+
+    } catch (err) {
+      console.log('Sorry, we couldn\'t find any trails!', err);
+      actualError.innerHTML = 'Sorry, we couldn\'t find any trails that you\'ve visited';
     }
-
-    const visitedTrails = await res.json();
-        allContainer.innerHTML = [visitedTrails[0].Trail.name];
-        console.log(visitedTrails)
 
   })
 
