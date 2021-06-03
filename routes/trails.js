1fea3fb1ed415ec076f3de5787efd3d7a20374c3
Geoffrey <geoffreyyang@gmail.com>
2021-06-03 17:58:28 -0400

    display current user

--- routes/trails.js
+++ routes/trails.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /***********************Internal Packages***********************/
 const { csrfProtection, asyncHandler } = require("../utils");
-const { Trail, State, Collection, Review } = require("../db/models");
+const { Trail, State,User, Collection, Review } = require("../db/models");
 const { requireAuth } = require("../auth");
 
 
@@ -16,15 +16,20 @@ const { requireAuth } = require("../auth");
 router.get("/:id(\\d+)", asyncHandler(async (req, res, next) => {
   const trailId = parseInt(req.params.id, 10);
   const trail = await Trail.findByPk(trailId, {
-    include: Review
+    include:
+      [
+        { model: Review, include: { model: User } }
+      ]
   });
-  console.log(trail.toJSON());
+  const loggedInUser=await User.findByPk(req.session.auth.userId)
   const state = await State.findByPk(trail.state_id);
+  console.log("who's here??????",loggedInUser.toJSON());
   req.session.save(() => {
     res.render("trail", {
       trail,
       state,
       title: "Trail",
+      loggedInUser: loggedInUser.toJSON()
     })
   });//end render
 }));//end GET route for a single trail
