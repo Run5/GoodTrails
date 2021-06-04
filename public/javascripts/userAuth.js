1f8c63d23528358ddde382c108234f2ecf27010f
Machaelmus <michaeljtufo@gmail.com>
2021-06-04 18:38:41 -0400

    Frontend JS complete for login/logout

--- public/javascripts/userAuth.js
+++ public/javascripts/userAuth.js
@@ -9,31 +9,39 @@ document.addEventListener('DOMContentLoaded', () => {
     /* LOGIN BUTTON */
     logoutButton.addEventListener('click', event => {
 
-        // modalDiv.createAttribute('style', 'display: block;')
     });
     loginButton.addEventListener('click', event => {
         loginButton.classList.add('on')
         modalDiv.setAttribute('style', 'display: block;')
-      
+        signupButton.setAttribute('disabled', 'disabled')
+
     })
     returnButton.addEventListener('click', event => {
         modalDiv.setAttribute('style', 'display: none;')
-        loginButton.classList.add('off')
+        loginButton.classList.remove('on')
+        signupButton.removeAttribute('disabled', 'disabled')
 
     })
     signupButton.addEventListener('click', event => {
         signupButton.classList.add('on')
         secondModal.setAttribute('style', 'display: block;')
+        loginButton.setAttribute('disabled', 'disabled')
 
     })
     secondReturnButton.addEventListener('click', event => {
         secondModal.setAttribute('style', 'display: none;')
-        signupButton.classList.add('off')
+        signupButton.classList.remove('on')
+        loginButton.removeAttribute('disabled', 'disabled')
 
     })
-    if(loginButton.classList === 'on') {
-        signupButton.disabled = true;
-    } else if (signupButton.classList === 'on') {
-        loginButton.disabled = true;
+    if(loginButton.classList.contains === 'on') {
+        signupButton.setAttribute('disabled', 'disabled')
+        loginButton.removeAttribute('disabled', 'disabled')
+        // signupButton.disabled = true;
+    } else if (signupButton.classList.contains === 'on') {
+        // loginButton.disabled = true;
+        loginButton.setAttribute('disabled', 'disabled')
+        signupButton.removeAttribute('disabled', 'disabled')
+
     }
 });
