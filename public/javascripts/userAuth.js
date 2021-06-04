24cefeafe1efa95daf531296b1cc3d137c399349
Machaelmus <michaeljtufo@gmail.com>
2021-06-04 13:13:49 -0400

    login, logout, and signup buttons added to the navbar | auth JS file added with all three buttons selected

---
+++ public/javascripts/userAuth.js
@@ -0,0 +1,7 @@
+document.getElementById('DOMContentLoaded', () => {
+    const loginButton = document.querySelector('.login');
+    const logoutButton = document.querySelector('.logout');
+    const signupButton = document.querySelector('.signup');
+
+    
+});
