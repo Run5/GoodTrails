b6d8aa8692ee0b23820475b2909e2bffed82035f
Chase Brashear <chase.brashear@gmail.com>
2021-08-25 08:59:27 -0700

    add demo user button and styling

---
+++ public/javascripts/forms.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", async () => {
+
+  const demoButton = document.querySelector('.demo');
+  const emailInput = document.querySelector('.email');
+  const pwInput = document.querySelector('.password');
+
+  demoButton.addEventListener("click", async () => {
+    emailInput.setAttribute('value','email@email.com');
+    pwInput.setAttribute('value','password');
+  });//endSubmitEventListener
+});//endDOMContentLoadedEventListener
